38f7cba9bea0c254b5fa6c23ab3ec15ef2304ed3
Ali Irawan <boylevantz@gmail.com>
2021-04-15 21:41:23 +0700

    add new template

---
+++ resources/views/layouts/default-with-left-panel.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+  <head>
+    <title>@yield("title")</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+  </head>
+  <body>
+      <div class="container">
+        
+        @yield("before-title")
+        
+        <h2>@yield("title")</h2>
+
+        <div class="row">
+          <div class="col-md-4">
+            
+            @yield("left-panel")
+            
+          </div>
+          <div class="col-md-8">
+            
+            @yield("content")
+
+          </div>
+        </div>
+      </div>
+  </body>
+</html>
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+  <head>
+    <title>@yield("title")</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+  </head>
+  <body>
+      <div class="container">
+        
+        @yield("before-title")
+        
+        <h2>@yield("title")</h2>
+
+        <div class="row">
+          <div class="col-md-12">
+            
+            @yield("content")
+
+          </div>
+        </div>
+      </div>
+  </body>
+</html>
